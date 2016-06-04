8e209e95138708a34ccc88156e290247b73c4f01
Aaron Cloutier <ascloutier@gmail.com>
2016-06-04 12:02:05 -0400

    Camera Controls Update
    Added Better zooming control with the camera and adjusted the scroll
    speed better.
    Also fixed error where the factory manager would access the placeholder
    object before it was created.

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -5,9 +5,13 @@ public class CameraControls : MonoBehaviour
 {
 
 	public float totalZoom = 2f;
-	public float zoomMax = 2.85f;
-	public float zoomMin = 0.1f;
+	public float maxZoom = 13f;
+	public float minZoom = -25f;
 	public float zoomModifier = 10f;
+    private float baseZoom = 0f;
+    private float currentZoom = 0f;
+    private float scaledCurrentZoom = 0f;
+    private float zoomFactor = 1.0f;
 
 	public float scrollSpeed = 25f;
 	public float scrollArea = 25f;
@@ -23,6 +27,10 @@ public class CameraControls : MonoBehaviour
 		Debug.Log ("center: " + center);
 		this.locationOffset = cameraLoc - center;
 		Debug.Log ("offset: " + locationOffset);
+        // distance from camera to origin plane
+        this.baseZoom = this.FindBaseZoom();
+        Debug.Log("distance: " + this.baseZoom);
+        this.currentZoom = this.baseZoom;
 	}
 
 	
@@ -32,17 +40,30 @@ public class CameraControls : MonoBehaviour
 
 		Vector3 translation = Vector3.zero;
 
-		float translate = Input.GetAxis ("Mouse ScrollWheel");
+		float translate = Input.GetAxis ("Mouse ScrollWheel") * this.zoomModifier;
 
-		if (totalZoom + translate > zoomMax) {
-			translate = zoomMax - totalZoom;
-		} else if (totalZoom + translate < zoomMin) {
-			translate = zoomMin - totalZoom;
-		}
+        if (translate != 0.0f)
+        {
+            //Debug.Log(translate);
+            
+            if (this.currentZoom + translate > this.maxZoom)
+            {
+                translate = this.maxZoom - this.currentZoom;
+            }
+            else if (this.currentZoom + translate < this.minZoom)
+            {
+                translate = this.minZoom - this.currentZoom;
+            }
+            
+            this.currentZoom += translate;
+            this.scaledCurrentZoom = this.currentZoom + 26;
 
-		totalZoom += translate;
+            this.zoomFactor = (this.baseZoom +26) / this.scaledCurrentZoom;
 
-		this.gameObject.transform.Translate (0, 0, translate * zoomModifier);
+            this.gameObject.transform.Translate(0, 0, translate);
+
+            Debug.Log(this.zoomFactor);
+        }
 
 		float xmouse = Input.mousePosition.x;
 		float ymouse = Input.mousePosition.y;
@@ -51,22 +72,22 @@ public class CameraControls : MonoBehaviour
 
 		if (xmouse < scrollArea && onScreen) {
 			// scroll left
-			translation += Vector3.right * -scrollSpeed * Time.deltaTime / totalZoom;
+			translation += Vector3.right * -scrollSpeed * Time.deltaTime * zoomFactor ;
 		}
 
 		if (xmouse > Screen.width - scrollArea && onScreen) {
 			// scroll right
-			translation += Vector3.right * scrollSpeed * Time.deltaTime / totalZoom;
+			translation += Vector3.right * scrollSpeed * Time.deltaTime * zoomFactor;
 		}
 
 		if (ymouse < scrollArea && onScreen) {
 			// scroll up
-			translation += Vector3.forward * -scrollSpeed * Time.deltaTime / totalZoom;
+			translation += Vector3.forward * -scrollSpeed * Time.deltaTime * zoomFactor;
 		}
 
 		if (ymouse > Screen.height - scrollArea && onScreen) {
 			// scroll down
-			translation += Vector3.forward * scrollSpeed * Time.deltaTime / totalZoom;
+			translation += Vector3.forward * scrollSpeed * Time.deltaTime * zoomFactor;
 		}
 
 		this.transform.position += translation;
@@ -80,6 +101,7 @@ public class CameraControls : MonoBehaviour
 	public void PointAt (GameObject a_object)
 	{
 		this.transform.position = a_object.transform.position + this.locationOffset;
+        this.currentZoom = this.baseZoom;
 	}
 	
 	private Vector3 FindCenterLoc ()
@@ -91,4 +113,16 @@ public class CameraControls : MonoBehaviour
 		Vector3 intersect = ray.origin - dirNorm * delta;
 		return intersect;
 	}
+
+    private float FindBaseZoom()
+    {
+        Plane plane = new Plane(Vector3.up, Vector3.up);
+        Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2);
+        Ray ray = Camera.main.ScreenPointToRay(center);
+        float distance;
+
+        plane.Raycast(ray, out distance);
+
+        return distance;
+    }
 }
